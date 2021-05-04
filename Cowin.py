b82fc182d1f92c229895f50536114ed732c148ff
Abhinav Mall <abhinav.0.mall@gmail.com>
2021-05-04 15:56:13 +0530

    Updating path

--- Cowin.py
+++ Cowin.py
@@ -7,7 +7,7 @@ from datetime import timedelta
 from dotenv import load_dotenv
 from pathlib import Path
 
-dotenv_path = Path('.env')
+dotenv_path = Path(".env")
 load_dotenv(dotenv_path=dotenv_path)
 
 # Your Account SID from twilio.com/console
